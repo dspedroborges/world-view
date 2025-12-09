8620e0f2ca06f2173ce4af4aacfd93fba3a91be6
dspedroborges <xpedrostewart@gmail.com>
2025-12-09 13:06:35 -0300

    cleanup: remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api-countries': {
-        target: 'https://www.apicountries.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-countries/, ''),
-      },
-    }
-  }
 })
